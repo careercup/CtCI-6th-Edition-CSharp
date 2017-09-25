5ce806d9efa3cf2e8a906f623fe852e7b26f421f
Xavier <xavierjohn@hotmail.com>
2017-09-25 01:03:22 -0700

    Chapter 4 Trees (#7)
    * Use abstract base Question class
    
    * Use hashset to check if characters in a string are unique
    
    * Use dictionary to check permutation
    
    * URLify
    
    * Bug fix Palindrome Permutation
    
    * Converted to .net core
    
    * Check balanced tree
    
    * 16.19 Pond size
    
    * Create Minimal BST from Sorted Unique Array
    
    * Replace value In Immutable Tree
    
    * Boggle
    
    * Q16_02_Word_Frequence
    
    * Q16_04_Tic_Tac_Toe CQRS style
    
    * Q16_06 Smallest Difference between pair in two array.
    
    * Convert Int to English
    
    * Q4_03_List_of_Depths
    
    * Is Balanced BST
    
    * Is Valid BST
    
    * Successor Node.
    
    * Lowest Common Ancestor with none BST.
    
    * Check Sub Tree
    
    * Use dictionary in Trie Node instead of List
    
    * Given a binary search tree with distinct elements,
    print all possible arrays that could have lead to this tree.

---
+++ Ch 04. Trees/Q4 09 BST Sequence.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ctci.Contracts;
+using ctci.Library;
+
+namespace Chapter04
+{
+    /// <summary>
+    /// Given a binary search tree with distinct elements,
+    /// print all possible arrays that could have lead to this tree.
+    /// </summary>
+    public class Q4_09_BST_Sequence : Question
+    {
+
+        public override void Run()
+        {
+            var root = Q4_02_CreateMinimalBSTfromSortedUniqueArray.Create(1, 2, 3);
+            BTreePrinter.Print(root);
+            var results = AllSequences(root);
+            foreach (var list in results)
+                AssortedMethods.PrintIntList(list);
+        }
+
+        public List<List<int>> AllSequences(TreeNode node)
+        {
+            var result = new List<List<int>>();
+
+            if (node == null)
+            {
+                result.Add(new List<int>());
+                return result;
+            }
+
+            var prefix = new List<int>();
+            prefix.Add(node.Data);
+
+            var leftSeq = AllSequences(node.Left);
+            var rightSeq = AllSequences(node.Right);
+
+            Weave(result, leftSeq, rightSeq, prefix);
+
+            return result;
+        }
+
+
+        private void Weave(List<List<int>> result, List<List<int>> leftSeq, List<List<int>> rightSeq, List<int> prefix)
+        {
+            foreach (var left in leftSeq)
+                foreach (var right in rightSeq)
+                {
+                    var weaved = new List<List<int>>();
+                    WeaveLists(left, right, weaved, prefix);
+                    result.AddRange(weaved);
+                }
+        }
+
+        /// <summary>
+        /// Weave lists together in all possible ways. This algorithm works by removing the head from one list,
+        /// recursing, and then doing the same thing wigth the other list.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="weaved"></param>
+        /// <param name="prefix"></param>
+        private void WeaveLists(List<int> first, List<int> second, List<List<int>> weaved, List<int> prefix)
+        {
+            // One list is empty. Add reminder to [a cloned] prefix and store result.
+            if (first.Count == 0 || second.Count == 0)
+            {
+                var result = prefix.ToList();
+                result.AddRange(first);
+                result.AddRange(second);
+                weaved.Add(result);
+                return;
+            }
+
+            // Recurse with head of first added to the prefix. Removing the head will damage
+            // first, so we'll need to put it back where we found it afterwards
+            MoveItemFromFirstListToPrefixAndWeave(first, second, weaved, prefix);
+
+            // Do the same thing with second, damaging and then restoring the list
+            MoveItemFromFirstListToPrefixAndWeave(second, first, weaved, prefix);
+        }
+
+        private void MoveItemFromFirstListToPrefixAndWeave(List<int> first, List<int> second, List<List<int>> weaved, List<int> prefix)
+        {
+            var headFirst = first[0];
+            first.RemoveAt(0);
+            prefix.Add(headFirst);
+            WeaveLists(first, second, weaved, prefix);
+            prefix.RemoveAt(prefix.Count - 1);
+            first.Insert(0, headFirst);
+        }
+    }
+}
